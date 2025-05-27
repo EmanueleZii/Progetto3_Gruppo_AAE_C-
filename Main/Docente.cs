c5e0555038735da30e3c211672cad61d4cdaec5b
Antonio Varchetta <avarchetta27@gmail.com>
2025-05-27 09:21:35 +0200

    Create Docente.cs
    Classe docente

---
+++ Main/Docente.cs
@@ -0,0 +1,16 @@
+using System;
+public class Docente
+{
+    public string Nome { get; set; }
+    public string Materia { get; set; }
+
+
+
+    public Docente(string nome, string materia)
+    {
+        Nome = nome;
+        Materia = materia;
+    }
+
+    
+}
